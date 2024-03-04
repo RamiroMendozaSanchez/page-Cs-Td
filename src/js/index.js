92968a3b07e0d3807cf42f767b2c9a3cd2feaae3
RamiroMendozaSanchez <ramiromendoza1612@gmail.com>
2024-03-04 00:56:41 -0600

    Transparencia al footer y justificado del texto promocionTxt

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
-// Configura la fecha y hora de finalización
-var fechaFinalizacion = new Date('2025-07-11T12:00:00').getTime();
+document.addEventListener("DOMContentLoaded", function () {
+  // Configura la fecha y hora de finalización
+  var fechaFinalizacion = new Date("2025-07-11T12:00:00").getTime();
 
-// Actualiza el contador cada segundo
-var x = setInterval(function() {
+  // Actualiza el contador cada segundo
+  var x = setInterval(function () {
     // Obtiene la fecha y hora actual
     var ahora = new Date().getTime();
 
@@ -11,19 +12,22 @@ var x = setInterval(function() {
 
     // Calcula días, horas, minutos y segundos
     var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    var horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var horas = Math.floor(
+      (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
     var segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
     // Actualiza los elementos HTML con el contador y las etiquetas
-    document.getElementById('dias').innerHTML = dias;
-    document.getElementById('horas').innerHTML = horas;
-    document.getElementById('minutos').innerHTML = minutos;
-    document.getElementById('segundos').innerHTML = segundos;
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
 
     // Si el tiempo ha terminado, muestra un mensaje
     if (diferencia < 0) {
-        clearInterval(x);
-        document.getElementById('contador').innerHTML = '¡Tiempo finalizado!';
+      clearInterval(x);
+      document.getElementById("contador").innerHTML = "¡Tiempo finalizado!";
     }
-}, 1000);
+  }, 1000);
+});
